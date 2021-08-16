ad7d9cd7df870c8a3e529a98f0effba771e0fa5e
Souvik Biswas <sbis1999@gmail.com>
2021-08-16 09:04:15 +0530

    Update integration test driver file

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -2,12 +2,16 @@ import 'dart:io';
 import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
-  await integrationDriver(
-    onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
-      final File image = File('$screenshotName.png');
-      image.writeAsBytesSync(screenshotBytes);
-      // Return false if the screenshot is invalid.
-      return true;
-    },
-  );
+  try {
+    await integrationDriver(
+      onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
+        final File image = await File('screenshots/$screenshotName.png')
+            .create(recursive: true);
+        image.writeAsBytesSync(screenshotBytes);
+        return true;
+      },
+    );
+  } catch (e) {
+    print('Error occured: $e');
+  }
 }
